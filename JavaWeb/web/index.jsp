0884ade53fbecb7b568bfebf7801e33a64f7b405
Firaymin <61828210+Firaymin@users.noreply.github.com>
2021-02-03 11:52:05 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 刘敏小可爱·
+  Date: 2021/2/2
+  Time: 10:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>刘敏</title>
+  </head>
+  <body>
+    <h1 style="color: aqua">hhhh</h1>
+  </body>
+</html>
---
+++ src/dddd/dddddd.java
@@ -0,0 +1,30 @@
+package dddd;
+
+
+import java.util.ArrayList;
+
+public class dddddd {
+    public static void main(String[] args){
+        System. out.println("HelloWorld!!");
+
+        ArrayList list=new ArrayList();
+        System.out.println( );
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            System.out.println("args = [" + args + "]");
+            System.out.println();
+            ArrayList list = new ArrayList();
+            list.add(123);
+            list.add(345);
+            list.add(567);
+            for (Object o : list) {
+                
+            }
+
+
+
+
+        }
+
+    }
+}
